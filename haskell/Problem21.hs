83609619166d76c1fd2fffcb4549f7e3108b2f7c
Riley Watkins <riley@rileywatkins.com>
2016-03-09 10:30:45 -0800

    Problem 21 in Haskell

---
+++ haskell/Problem21.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import qualified Data.Map as Map
+
+readInt :: String -> Int
+readInt = read
+
+theNumber :: Integer
+theNumber = 220
+
+isDivisor :: Integer -> Integer -> Bool
+isDivisor number i = (mod number i) == 0
+
+divisors number = filter (isDivisor number) [1..(number `div` 2)]
+
+sumDivisors = sum . divisors
+
+compute :: Map.Map Integer Integer -> Integer -> Map.Map Integer Integer
+compute memo i = Map.insert i (sumDivisors i) memo
+
+buildMap :: Map.Map Integer Integer
+buildMap = foldl compute Map.empty [1..10000]
+
+amicableNumbers = let values = buildMap
+                  in [ (x,y) | x <- [1..10000],
+                               y <- [1..10000],
+                               x < y,
+                               ((Map.!) values x) == y,
+                               ((Map.!) values y) == x ]
+
+main = do
+  putStrLn "Problem 21"
+  let nums = amicableNumbers
+  putStrLn $ show $ nums
+  putStrLn $ show $ foldl (\acc (i,j) -> acc + i + j) 0 nums
