89bf4b622b1efbad908a4eb2a16205fb83e9b28e
Riley Watkins <riley@rileywatkins.com>
2016-03-20 11:29:56 -0700

    Clean up

--- haskell/Problem25/src/Lib.hs
+++ haskell/Problem25/src/Lib.hs
@@ -9,9 +9,6 @@ fibs = map fst $ iterate (\(a,b) -> (b,a+b)) (0,1)
 --fibs = unfoldr (\(a, b) -> Just (a, (b, a+b))) (0, 1)
 --fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-fib :: Int -> Integer
-fib n = fibs !! n
-
 enumerate :: [a] -> [(Integer,a)]
 enumerate = enumerate' 0
   where enumerate' :: Int -> [a] -> [(Integer,a)]
@@ -20,10 +17,6 @@ enumerate = enumerate' 0
         enumerate' i (x:xs) = (toInteger i, x) : enumerate' (i + 1) xs
 
 answer :: Int -> (Integer,Integer)
-answer digits = head .
-                dropWhile (not . valid) .
-                map (\(i, x) -> (i + 1, x)) .
-                enumerate $
-                map fib [1..]
+answer digits = head . dropWhile (not . valid) . enumerate $ fibs
   where valid :: (Integer,Integer) -> Bool
         valid = (== digits) . length . show . snd
