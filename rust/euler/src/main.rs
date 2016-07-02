6d6ac81fac98dcbb1bf2f6037b8830fa0afa9823
Riley Watkins <riley@rileywatkins.com>
2016-07-02 15:56:51 -0700

    Problem 2 in Rust

--- rust/euler/src/main.rs
+++ rust/euler/src/main.rs
@@ -4,7 +4,29 @@ fn problem1() -> u64 {
         .fold(0, |acc, n| acc + n)
 }
 
+
+fn problem2() -> u64 {
+    let mut a = 1;
+    let mut b = 2;
+    let mut sum = 0;
+
+    while b < 4_000_000 {
+        if b % 2 == 0 {
+            sum += b;
+        }
+        let tmp = a;
+        a = b;
+        b = tmp + b;
+    }
+
+    sum
+}
+
+
 fn main() {
     println!("Problem 1");
     println!("{}", problem1());
+
+    println!("Problem 2");
+    println!("{}", problem2());
 }
